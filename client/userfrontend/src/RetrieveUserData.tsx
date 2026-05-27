133017f4ac6557ea78720f241d6509130aa56202
Ted95153420 <tedmilne@proton.me>
2026-05-27 14:33:13 -0700

    UI displaye more fields

--- client/userfrontend/src/RetrieveUserData.tsx
+++ client/userfrontend/src/RetrieveUserData.tsx
@@ -25,7 +25,10 @@ function DisplayData() {
                 data.users.map((user) => {
                     return (
                         <div key={user.username}>
-                            <h3>Name: {user.name}</h3>
+                            <h2>Name: {user.name}</h2>
+                            <h3>Age: {user.age}</h3>
+                            <h3>Username: {user.username}</h3>
+                            <h3>Nationality: {user.nationality}</h3>
                         </div>
                     );
                 })}

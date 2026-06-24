066326a831c3a28f2b9543cbbf09c3c8b533da69
Ted95153420 <tedmilne@proton.me>
2026-06-24 08:17:37 -0700

    consumer changed so it will run as a webservice

--- streaming/consumer/consumer.js
+++ streaming/consumer/consumer.js
@@ -1,6 +1,46 @@
+/**
+ * TEMPORARY RENDER WORKAROUND
+ *
+ * This consumer was originally intended to run as a true background worker that
+ * continuously listens for Kafka / Redpanda messages and writes to the database.
+ *
+ * However, Render charges for Background Workers, and that cost is currently a
+ * constraint for this project.
+ *
+ * As a workaround, this consumer has been modified to also behave like a very
+ * small web service by exposing an HTTP endpoint. This allows it to be deployed
+ * on Render as a Web Service instead of a paid Background Worker.
+ *
+ * IMPORTANT:
+ * - The HTTP server in this file exists only to satisfy Render's expectation
+ *   that a web service binds to a port.
+ * - The real purpose of this process is still to consume Kafka messages.
+ * - This is a cost-saving workaround, not the ideal long-term architecture.
+ *
+ * Longer term, if budget allows, this should probably be moved back to a proper
+ * Background Worker style service and the HTTP server removed.
+ */
+
+const express = require("express");
 const { createUser } = require("../../server/services/userService");
 require("dotenv").config();
 const { Kafka } = require("kafkajs");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+// TODO: This HTTP endpoint was added purely so Render can treat this process
+// as a Web Service. Remove this if/when the consumer is deployed as a true
+// Background Worker.
+app.get("/", (req, res) => {
+    res.send("ResolveIQ consumer is running.");
+});
+
+// TODO: This HTTP listener was added purely for the Render Web Service
+// workaround. Remove this if/when the consumer runs as a proper Background Worker.
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`HTTP server listening on port ${PORT}`);
+});
 
 const kafka = new Kafka({
     clientId: "resolveiq-consumer",
@@ -11,7 +51,12 @@ const consumer = kafka.consumer({ groupId: "resolveiq-test-group" });
 
 async function run() {
     await consumer.connect();
-    await consumer.subscribe({ topic: "user-events", fromBeginning: true });
+
+    // TODO: We are using fromBeginning: false here because when running as a
+    // Render Web Service, restarts/spin-downs could cause old messages to be
+    // replayed unnecessarily. Revisit this setting later when running as a
+    // proper always-on background service.
+    await consumer.subscribe({ topic: "user-events", fromBeginning: false });
 
     console.log("Consumer is listening on topic: user-events");
 

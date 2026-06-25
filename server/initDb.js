432424624d444c14115d4cc38a6ba5d6d75a7244
Ted95153420 <tedmilne@proton.me>
2026-06-25 15:59:15 -0700

    changed databse table structure. Have a place to store transaction data

--- server/initDb.js
+++ server/initDb.js
@@ -5,17 +5,31 @@ const pool = require("./db");
 
 async function init() {
     await pool.query(`
+
+    DROP TABLE IF EXISTS transactions;
+        DROP TABLE IF EXISTS users;
+
     CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY,
-      name TEXT NOT NULL,
-      username TEXT NOT NULL,
-      age INTEGER NOT NULL,
-      nationality TEXT NOT NULL,
-      loyaltypointbalance INTEGER NOT NULL DEFAULT 0
-    )
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            username TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            nationality TEXT NOT NULL
+            loyaltypointbalance INTEGER NOT NULL DEFAULT 0
+        );
+
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            gametype TEXT NOT NULL,
+            amountwagered NUMERIC(10,2) NOT NULL,
+            transaction_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        );
+
   `);
 
-    console.log("Users table is ready.");
+    console.log("Users and transactionstable is ready.");
     await pool.end();
 }
 

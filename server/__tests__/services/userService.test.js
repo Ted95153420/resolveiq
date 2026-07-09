cf7b25b77e4c6f0c6fef61991f798b12fc964a1e
Ted95153420 <tedmilne@proton.me>
2026-07-09 08:23:45 -0700

    user and transaction tests now have a reasonable coverage

--- server/__tests__/services/userService.test.js
+++ server/__tests__/services/userService.test.js
@@ -23,26 +23,48 @@ const {
     recordProcessedEvent,
 } = require("../../repositories/processedEventRepository");
 
- const existingUser = {
-            id: 1,
-            name: "Existing User",
-            username: "demo123",
-            age: 40,
-            nationality: "CANADA",
-            loyaltypointbalance: 500,
-        };
+const existingUser = {
+    id: 1,
+    name: "Existing User",
+    username: "demo123",
+    age: 40,
+    nationality: "CANADA",
+    loyaltypointbalance: 500,
+};
 
-        const event = {
-            eventId: "event-123",
-            eventType: "user.created",
-            payload: {
-                name: "New User",
-                username: "demo123",
-                age: 25,
-                nationality: "CANADA",
-                loyaltypointbalance: 100,
-            },
-        };
+const existingUserEvent = {
+    eventId: "event-123",
+    eventType: "user.created",
+    payload: {
+        name: "New User",
+        username: "demo123",
+        age: 25,
+        nationality: "CANADA",
+        loyaltypointbalance: 100,
+    },
+};
+
+const newUser = {
+    id: 2,
+    name: "New User",
+    username: "demo124",
+    age: 27,
+    nationality: "CANADA",
+    loyaltypointbalance: 0,
+};
+
+const newUserEvent = {
+    eventId: "event-124",
+    eventType: "user.created",
+    payload: {
+        name: "New User",
+        username: "demo124",
+        age: 27,
+        nationality: "CANADA",
+        loyaltypointbalance: 0,
+    },
+};
+
 
 describe("createUser", () => {
     beforeEach(() => {
@@ -52,7 +74,7 @@ describe("createUser", () => {
     test("create User returns existing user returned if already in database", async () => {
         hasProcessedEvent.mockResolvedValue(false);
         getUserByUsername.mockResolvedValue(existingUser);
-        const result = await createUser(event);
+        const result = await createUser(existingUserEvent);
 
         expect(result).toEqual(existingUser);
     });
@@ -60,7 +82,7 @@ describe("createUser", () => {
     test("createUser does not call addUser when existing user detected", async () => {
         hasProcessedEvent.mockResolvedValue(false);
         getUserByUsername.mockResolvedValue(existingUser);
-        await createUser(event);
+        await createUser(existingUserEvent);
 
         expect(addUser).not.toHaveBeenCalled();
     });
@@ -69,9 +91,31 @@ describe("createUser", () => {
         hasProcessedEvent.mockResolvedValue(false);
         getUserByUsername.mockResolvedValue(existingUser);
 
-        await createUser(event);
+        await createUser(existingUserEvent);
 
         expect(recordProcessedEvent).toHaveBeenCalledWith("event-123", "user.created");
 
     });
+
+    test("createUser does call addUser when user does not pre exist", async () => {
+        hasProcessedEvent.mockResolvedValue(false);
+        getUserByUsername.mockResolvedValue(null);
+
+        await createUser(newUserEvent);
+
+        expect(addUser).toHaveBeenCalled()
+
+    });
+
+    test("createUser does record event when user does not pre exist", async () => {
+        hasProcessedEvent.mockResolvedValue(false);
+        getUserByUsername.mockResolvedValue(null);
+
+        await createUser(newUserEvent);
+
+        expect(recordProcessedEvent).toHaveBeenCalledWith("event-124", "user.created");
+
+    });
+
+
 });

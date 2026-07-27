7ee892cdc2d6c53b52969ed259ad09e50ba51e02
Ted95153420 <tedmilne@proton.me>
2026-07-27 14:51:02 -0700

    configure Apollo Client for GraphQL sunscriptions

--- client/userfrontend/src/app/App.tsx
+++ client/userfrontend/src/app/App.tsx
@@ -2,18 +2,59 @@ import './App.css'
 
 import {
     ApolloClient,
-    InMemoryCache,
-    HttpLink
+    ApolloLink,
+    HttpLink,
+    InMemoryCache,  
 } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client/react';
+
+import {
+    createClient,
+} from "graphql-ws";
+
+import { 
+    ApolloProvider 
+} from '@apollo/client/react';
+
+import {
+    GraphQLWsLink,
+} from "@apollo/client/link/subscriptions";
+
+import {
+    OperationTypeNode,
+} from "graphql";
 
 import AppRoutes from "./routes";
 
+const graphqlHttpUrl =
+    import.meta.env.VITE_GRAPHQL_API_URL ||
+    "http://localhost:4000/graphql";
+
+const graphqlWebSocketUrl =
+    import.meta.env.VITE_GRAPHQL_WS_URL ||
+    "ws://localhost:4000/graphql";
+
+const httpLink = new HttpLink({
+    uri: graphqlHttpUrl
+});
+
+const webSocketLink = new GraphQLWsLink(
+    createClient({
+        url: graphqlWebSocketUrl,
+        lazy: true,
+    })
+);
+
+const splitLink = ApolloLink.split(
+    ({ operationType }) =>
+        operationType === OperationTypeNode.SUBSCRIPTION,
+
+    webSocketLink,
+    httpLink
+);
+
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: import.meta.env.VITE_GRAPHQL_API_URL || "http://localhost:4000/graphql"
-    })
+    link: splitLink,
 });
 
 function App() { 
